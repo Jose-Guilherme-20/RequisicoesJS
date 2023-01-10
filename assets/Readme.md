a588b6698da9b0ab62d2f952d0b6bfff6d56ac2e
José-Guilherme-20 <jgscosta20@gmail.com>
2023-01-10 08:42:06 -0300

    Requsições javascript

---
+++ assets/Readme.md
@@ -0,0 +1,9 @@
+<h1>Requisições</h1>
+
+<ul>
+        <li>Sicrono e Assicrono</li>
+         <li>Req e Resp</li>
+          <li> Headers e Body</li>
+           <li>Api</li>
+
+</ul>
---
+++ assets/javascript/sicrono.js
@@ -0,0 +1,13 @@
+// O que é Sincrono e Assicrono?
+
+// Sicronomo executa um código para depois ir para outro.
+// Assicrono executa um codigo enquanto um não está pronto passa para o proximo.
+
+//Como a web funciona(Requisição e Resposta)?
+// request = requisição
+// Cabeçalhos / Headers
+// Corpo / Body
+
+// Response = Resposta
+// Cabeçalhos / Headers
+//  Corpo / Body
